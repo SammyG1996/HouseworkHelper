809c921eaa0abb22c4e77e51dee94e8a7bc9466f
Samuel Gonzalez Jr <95721703+SammyG1996@users.noreply.github.com>
2023-09-02 10:04:28 -0400

    updated about pages

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,22 +2,29 @@ import React from 'react'
 
 export const About = () => {
   return (
-    <div className='w-[80vw] flex justify-center'>
-        <p>🌟 What a Mess! 🌟
-Tired of the constant struggle to keep those household chores in check? 🧹🏡
-Say goodbye to the chaos with Housework Helper, your ultimate web app solution! 🚀
-Household chores can be a real headache, especially when everyone in the family is involved. But fear not, because Housework Helper is here to save the day! 💪✨
-🌟 What is Housework Helper? 🌟
-Housework Helper is not just a web app; it's your secret weapon for conquering those chores effortlessly. 💻✅
-Imagine this: You log in, create a new task with a few clicks, assign it to your family member(s), choose a due date, and voilà! ✨📅
-But that's not all - Housework Helper takes care of the rest! 🤖📢
-It sends gentle reminders to those responsible, ensuring no task ever slips through the cracks again. 📩🔔
-And for those recurring chores? No problem! Housework Helper lets you set the interval and then...just set it and forget it! 🔄🤯
-No more racking your brain about when you last tackled that dusty couch or cleaned out the fridge. 🛋️🍽️
-Housework Helper's got your back, so you can enjoy a tidy, stress-free home without the hassle. 🌟🏠
-Don't let chores bring you down; let Housework Helper bring the sparkle back to your home life online! 💎✨
-Sign up today and reclaim your time for the things you love! 🌐🌈
-</p>
+    <div className='w-[100vw] flex justify-center'>
+        <div className='mt-10 w-[80vw] flex flex-col'>
+            <h1 className='text-[#004C6C] font-serif text-3xl lg:text-4xl'>What a Mess!</h1>
+            <p className='font-serif mt-5'>Tired of the constant struggle to keep those household chores in check? 🧹🏡</p>
+            <p className='font-serif mt-5'>Say goodbye to the chaos with Housework Helper, your ultimate web app solution! 🚀</p>
+            <p className='font-serif mt-5'>Household chores can be a real headache, especially when everyone in the family is involved.</p>
+            <p className='font-serif mt-5'>But fear not, because Housework Helper is here to save the day! 💪✨</p>
+            <h1 className='text-[#004C6C] font-serif text-2xl lg:text-4xl mt-10'>What's Housework Helper?</h1>
+            <p className='font-serif mt-5'>Housework Helper is not just a web app; it's your secret weapon for conquering those chores effortlessly. 💻✅</p>
+            <p className='font-serif mt-5'>Imagine this: You log in, create a new task with a few clicks, assign it to your family member(s), choose a due date, and voilà! ✨📅</p>
+            <p className='font-serif mt-5'>But that's not all - Housework Helper takes care of the rest! 🤖📢</p>
+            <p className='font-serif mt-5'>It sends gentle reminders to those responsible, ensuring no task ever slips through the cracks again. 📩🔔</p>
+            <p className='font-serif mt-5'>And for those recurring chores? No problem! Housework Helper lets you set the interval and then...just set it and forget it! 🔄🤯</p>
+            <p className='font-serif mt-5'>No more racking your brain about when you last tackled that dusty couch or cleaned out the fridge. 🛋️🍽️</p>
+            <p className='font-serif mt-5'>Housework Helper's got your back, so you can enjoy a tidy, stress-free home without the hassle. 🌟🏠</p>
+            <p className='font-serif mt-5'>Don't let chores bring you down; let Housework Helper bring the sparkle back to your home life online! 💎✨</p>
+            <p className='font-serif mt-5'>Sign up today and reclaim your time for the things you love! 🌐🌈</p>
+
+        </div>
+        
+        
+
+
     </div>
   )
 }
