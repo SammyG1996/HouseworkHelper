9d6427478bb35b4a69f6818bbacfcb713ac90512
Samuel Gonzalez Jr <95721703+SammyG1996@users.noreply.github.com>
2023-09-03 16:42:30 -0400

    updated more styling and font size

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -32,17 +32,19 @@ export const Home = () => {
             WELCOME TO
             </Typography>
             <img src={Logo} className="w-[60%] mx-auto" alt="" />
-            <Typography className='font-logo font-thin text-[#072E33] mt-5'>
+            <Typography className='font-sans text-[#072E33] mt-5'>
             Our mission is to simplify your household tasks.
             </Typography>
-            <Typography className='font-logo font-thin text-[#072E33]'>
-            Sign up today!
+            <Typography className='font-sans text-[#072E33]'>
+            <Button className='w-[155px] font-logo font-thin mt-2 mx-1 bg-[#6DA5C0] shadow-lg' onClick={()=> navigate('/signup')} >Sign Up Today‼️</Button>
+            <Button className='w-[120px] font-logo font-thin mt-2 mx-1 bg-[#6DA5C0] shadow-lg' onClick={()=> navigate('/signup')} >Or Login 😁</Button>
             </Typography>
           </CardBody>
           <CardFooter className="pt-0">
-            <Button className='w-[110px] font-logo font-thin mt-2 mx-1 bg-[#6DA5C0] shadow-lg' onClick={()=> navigate('/signup')} >Sign Up</Button>
-            <Button className='w-[110px] font-logo font-thin mt-2 mx-1 bg-[#6DA5C0] shadow-lg' onClick={()=> navigate('/signin')} >Sign In</Button>
-            <Button className='w-[150px] font-logo font-thin mt-2 mx-1 bg-[#6DA5C0] shadow-lg' onClick={()=> navigate('/learn-more')} >Learn More</Button>
+            
+            {/* <Button className='w-[110px] font-logo font-thin mt-2 mx-1 bg-[#6DA5C0] shadow-lg' onClick={()=> navigate('/signin')} >Sign In</Button> */}
+            {/* <Button className='w-[150px] font-logo font-thin mt-2 mx-1 bg-[#6DA5C0] shadow-lg' onClick={()=> navigate('/learn-more')} >Learn More</Button> */}
+            <p><a onClick={()=> navigate('/learn-more')} className='underline text-[#6DA5C0]'>Learn More Here</a></p>
           </CardFooter>
         </Card>
 
